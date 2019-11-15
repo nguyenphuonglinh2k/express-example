c5f0ce4c6bc53508c1497f3449ca279625e79e5e
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2019-11-15 21:42:45 +0700

    csrt middleware

---
+++ routes/product.route.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var controller = require('../controllers/product.controller');
+
+var router = express.Router();
+
+router.get('/', controller.showProduct);
+
+module.exports = router;
